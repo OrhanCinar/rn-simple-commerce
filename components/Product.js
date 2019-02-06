7725b2dfc96b0fce0e10017b6218bf3fda893b5b
Orhan CINAR <orhancinar35@gmail.com>
2019-02-06 01:30:11 +0100

    update product

--- components/Product.js
+++ components/Product.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { View, Text, TextInput, Image, StyleSheet, Button } from "react-native";
 
 class Product extends Component {
   state = {};
   render() {
-    return <div />;
+    return (
+      <View>
+        <Image accessibilityLabel="product Image" />
+        <Text>PRODUCT_NAME</Text>
+        <Text>OLD_PRICE</Text>
+        <Text>PRICE</Text>
+        {/* ADD SPINNER FOR QUANTITY */}
+        <Image accessibilityLabel="favorite" />
+        <Text>DESCRIPTION</Text>
+        <Button>ADD TO CART</Button>
+      </View>
+    );
   }
 }
 
