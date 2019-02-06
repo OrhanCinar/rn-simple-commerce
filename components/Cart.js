c3b4f98d59e0a414ef0a00805867706012eaac2b
Orhan CINAR <orhancinar35@gmail.com>
2019-02-06 01:29:29 +0100

    update cart

--- components/Cart.js
+++ components/Cart.js
@@ -1,9 +1,37 @@
 import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  StyleSheet,
+  Button,
+  ScrollView
+} from "react-native";
 
 class Cart extends Component {
   state = {};
   render() {
-    return <div>Cart</div>;
+    return (
+      <View>
+        <ScrollView>
+          <View>
+            <Image accessibilityLabel="PRODUCT IMAGE" />
+            <Text>PRODUCT NAME</Text>
+            <Text>PRODUCT Price</Text>
+            <Text>PRODUCT Quantity</Text>
+            <Text>PRODUCT Total</Text>
+            <Button>REMOVE ITEM</Button>
+          </View>
+        </ScrollView>
+
+        <View>
+          <Text>TOTAL</Text>
+          <Text>DISCOUNT</Text>
+          <Text>SUB TOTAL</Text>
+        </View>
+      </View>
+    );
   }
 }
 
