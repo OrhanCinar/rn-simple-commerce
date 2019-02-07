3591e211e71ff22a2de57c6f6a786e1ac30df2b0
Orhan CINAR <orhancinar35@gmail.com>
2019-02-07 23:40:20 +0100

    cleanup

--- components/Main.js
+++ components/Main.js
@@ -9,73 +9,8 @@ import {
 } from "react-native";
 
 class Main extends React.Component {
-  state = {
-    products: [
-      {
-        id: 32740307068,
-        name: "Sandisk 64gb SdCard",
-        price: 10,
-        imageUrl: "../assets/Products/Images/32740307068_01.jpg"
-      },
-      {
-        id: 32880261657,
-        name: "Lingerie",
-        price: 23,
-        imageUrl: "../assets/Products/Images/32740307068_01.jpg"
-      },
-      {
-        id: 32903877682,
-        name: "Samsung Screen Protection",
-        price: 8.99,
-        imageUrl: "../assets/Products/Images/32740307068_01.jpg"
-      },
-      {
-        id: 32924478819,
-        name: "Man Bag",
-        price: 18.5,
-        imageUrl: "../assets/Products/Images/32740307068_01.jpg"
-      },
-      {
-        id: 32947594040,
-        name: "Utilities",
-        price: 14.25,
-        imageUrl: "../assets/Products/Images/32740307068_01.jpg"
-      }
-    ]
-  };
+  state = {};
   render() {
-    // let renderPhotoTypes = () => {
-    //   let type = [];
-
-    //   this.state.products.map(product => {
-    //     type.push(
-    //       <Image
-    //         key={product.id}
-    //         source={{ source: product.imageUrl }}
-    //         // style={{ width: 400, height: 400 }}
-    //       />
-    //     );
-    //   });
-    //   console.log(type);
-    //   return type;
-    // };
-
-    //return <View>{renderPhotoTypes()}</View>;
-
-    // return (
-    //   <ListView
-    //     dataSource={this.state.products}
-    //     renderRow={data => {
-    //       <View>
-    //         <Image
-    //           source={require("../assets/Products/Images/32947594040_01.jpg")}
-    //           style={{ width: 200, height: 200 }}
-    //         />
-    //       </View>;
-    //     }}
-    //   />
-    // );
-
     return (
       <View style={styles.headerContainer}>
         <Text style={styles.headerText}>Most Selling Products</Text>
