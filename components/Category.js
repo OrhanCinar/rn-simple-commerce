9c0ef49b0a8e34b29e22b493f7a5b2bc65c8afc4
Orhan CINAR <orhancinar35@gmail.com>
2019-02-07 23:40:07 +0100

    update category

--- components/Category.js
+++ components/Category.js
@@ -6,12 +6,47 @@ class Category extends Component {
   render() {
     return (
       <View>
-        <Image>BANNER</Image>
+        <Image
+          source={require("../assets/Banners/HTB1oXyyaZrrK1RjSspa763REXXa5.png")}
+          style={{ height: 50, width: 200 }}
+        />
 
         <ScrollView>
           <View>
-            <Image />
-            <Text>CATEGORY NAME</Text>
+            <Image
+              style={{ height: 50, width: 50 }}
+              source={require("../assets/Icons/bags.png")}
+            />
+            <Text>BAGS</Text>
+          </View>
+          <View>
+            <Image
+              style={{ height: 50, width: 50 }}
+              source={require("../assets/Icons/dress.png")}
+            />
+            <Text>WOMEN DRESS</Text>
+          </View>
+          <View>
+            <Image
+              style={{ height: 50, width: 50 }}
+              source={require("../assets/Icons/tools.png")}
+            />
+            <Text>TOOLS</Text>
+          </View>
+
+          <View>
+            <Image
+              style={{ height: 50, width: 50 }}
+              source={require("../assets/Icons/smartphone.png")}
+            />
+            <Text>SMARTPHONE</Text>
+          </View>
+          <View>
+            <Image
+              style={{ height: 50, width: 50 }}
+              source={require("../assets/Icons/computer.png")}
+            />
+            <Text>COMPUTERS</Text>
           </View>
         </ScrollView>
       </View>
