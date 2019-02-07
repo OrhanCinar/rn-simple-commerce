7eba11bf2867dcfefeecd281b964a0404334d265
Orhan CINAR <orhancinar35@gmail.com>
2019-02-07 23:39:46 +0100

    add data file

---
+++ data.js
@@ -0,0 +1,32 @@
+products: [
+  {
+    id: 32740307068,
+    name: "Sandisk 64gb SdCard",
+    price: 10,
+    imageUrl: "../assets/Products/Images/32740307068_01.jpg"
+  },
+  {
+    id: 32880261657,
+    name: "Lingerie",
+    price: 23,
+    imageUrl: "../assets/Products/Images/32740307068_01.jpg"
+  },
+  {
+    id: 32903877682,
+    name: "Samsung Screen Protection",
+    price: 8.99,
+    imageUrl: "../assets/Products/Images/32740307068_01.jpg"
+  },
+  {
+    id: 32924478819,
+    name: "Man Bag",
+    price: 18.5,
+    imageUrl: "../assets/Products/Images/32740307068_01.jpg"
+  },
+  {
+    id: 32947594040,
+    name: "Utilities",
+    price: 14.25,
+    imageUrl: "../assets/Products/Images/32740307068_01.jpg"
+  }
+];
