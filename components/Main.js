b61cb6bc0b7dfff2d59af559e94737e7b553d7b6
Orhan CINAR <orhancinar35@gmail.com>
2019-02-09 00:56:36 +0100

    update main

--- components/Main.js
+++ components/Main.js
@@ -1,13 +1,6 @@
 import React from "react";
-import {
-  View,
-  Text,
-  Image,
-  ListView,
-  StyleSheet,
-  ScrollView
-} from "react-native";
-
+import { View, Text, Image, ListView, ScrollView } from "react-native";
+import styles from "./styles/Main.style";
 class Main extends React.Component {
   state = {};
   static navigationOptions = {
@@ -97,69 +90,3 @@ class Main extends React.Component {
 }
 
 export default Main;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    flexWrap: "wrap",
-    flexDirection: "row",
-    marginLeft: 2,
-    marginRight: 2
-  },
-  productContainer: {
-    marginBottom: 10,
-    marginLeft: 2,
-    marginTop: 10,
-    borderRadius: 8,
-    borderWidth: 0.9,
-    borderColor: "#FF99CC"
-  },
-  productImage: {
-    justifyContent: "space-between",
-    marginBottom: 10,
-    width: 198,
-    height: 198
-  },
-  productTextContainer: {
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  productHeader: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#808080"
-  },
-  //PRICE
-  priceContainer: {
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  price: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "green"
-  },
-  oldPrice: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "red",
-    textDecorationLine: "line-through",
-    textDecorationStyle: "solid",
-    fontStyle: "italic"
-  },
-
-  //HEADER
-  headerContainer: {
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  headerText: {
-    color: "#FF007F",
-    fontWeight: "bold",
-    fontSize: 25,
-    marginTop: 50,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
