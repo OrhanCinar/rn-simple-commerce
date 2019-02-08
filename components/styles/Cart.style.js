beb1252f81c807fa09caf041f9b81284c1f164ff
Orhan CINAR <orhancinar35@gmail.com>
2019-02-09 00:56:59 +0100

    add cart style

---
+++ components/styles/Cart.style.js
@@ -0,0 +1,49 @@
+import { StyleSheet } from "react-native";
+
+export default (styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //backgroundColor: "#fff",
+    flexWrap: "wrap",
+    flexDirection: "column",
+    marginTop: 40,
+    marginLeft: 5,
+    marginRight: 5,
+    justifyContent: "space-between"
+  },
+  productItem: {
+    //marginBottom: 10,
+    width: 200,
+    height: 200
+  },
+  imageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    borderWidth: 0.9,
+    borderColor: "#202020",
+    marginBottom: 5
+  },
+  productHeader: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+
+  priceContainer: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  price: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "green"
+  },
+  oldPrice: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "red",
+    textDecorationLine: "line-through",
+    textDecorationStyle: "solid",
+    fontStyle: "italic"
+  }
+}));
