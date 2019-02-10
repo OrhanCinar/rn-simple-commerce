09d323b888e061c272f316bc97eb8e19421c57ec
Orhan CINAR <orhancinar35@gmail.com>
2019-02-10 01:03:28 +0100

    update style

--- components/styles/Product.style.js
+++ components/styles/Product.style.js
@@ -7,8 +7,8 @@ export default (styles = StyleSheet.create({
     flexWrap: "wrap",
     flexDirection: "column",
     marginTop: 40,
-    marginLeft: 5,
-    marginRight: 5,
+    //marginLeft: 5,
+    //marginRight: 5,
     justifyContent: "space-between"
   },
   productItem: {
@@ -19,14 +19,27 @@ export default (styles = StyleSheet.create({
   imageContainer: {
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 8,
-    borderWidth: 0.9,
+    // borderRadius: 8,
+    // borderWidth: 0.9,
     borderColor: "#202020",
-    marginBottom: 5
+    marginBottom: 5,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 1
+  },
+  headerContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 1
   },
   productHeader: {
     fontSize: 30,
-    fontWeight: "bold"
+    //fontWeight: "bold",
+    textShadowColor: "rgba(0, 0, 0, 0.75)",
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10
   },
   btnAdd: {
     fontSize: 24,
@@ -37,7 +50,10 @@ export default (styles = StyleSheet.create({
   },
   priceContainer: {
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 1
   },
   price: {
     fontSize: 30,
