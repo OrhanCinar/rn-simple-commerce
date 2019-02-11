566682276bd5ea3b36a96641ff162f2c69897232
Orhan CINAR <orhancinar35@gmail.com>
2019-02-11 01:31:35 +0100

    update category

--- components/Category.js
+++ components/Category.js
@@ -1,49 +1,53 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import styles from "./styles/Category.style";
 
 class Category extends React.Component {
   state = {};
+  static navigationOptions = {
+    title: "Categories"
+  };
   render() {
     return (
       <View>
         <Image
           source={require("../assets/Banners/HTB1oXyyaZrrK1RjSspa763REXXa5.png")}
-          style={{ height: 50, width: 200 }}
+          style={{ height: 200, width: 400 }}
         />
 
         <ScrollView>
-          <View>
+          <View style={styles.categoryContainer}>
             <Image
-              style={{ height: 50, width: 50 }}
+              style={{ height: 100, width: 100 }}
               source={require("../assets/Icons/bags.png")}
             />
             <Text>BAGS</Text>
           </View>
-          <View>
+          <View style={styles.categoryContainer}>
             <Image
-              style={{ height: 50, width: 50 }}
+              style={{ height: 100, width: 100 }}
               source={require("../assets/Icons/dress.png")}
             />
             <Text>WOMEN DRESS</Text>
           </View>
-          <View>
+          <View style={styles.categoryContainer}>
             <Image
-              style={{ height: 50, width: 50 }}
+              style={{ height: 100, width: 100 }}
               source={require("../assets/Icons/tools.png")}
             />
             <Text>TOOLS</Text>
           </View>
 
-          <View>
+          <View style={styles.categoryContainer}>
             <Image
-              style={{ height: 50, width: 50 }}
+              style={{ height: 100, width: 100 }}
               source={require("../assets/Icons/smartphone.png")}
             />
             <Text>SMARTPHONE</Text>
           </View>
-          <View>
+          <View style={styles.categoryContainer}>
             <Image
-              style={{ height: 50, width: 50 }}
+              style={{ height: 100, width: 100 }}
               source={require("../assets/Icons/computer.png")}
             />
             <Text>COMPUTERS</Text>
