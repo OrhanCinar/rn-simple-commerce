223d040e565380a5d6a17c25d927b72807e40d69
Orhan CINAR <orhancinar35@gmail.com>
2019-02-28 02:24:23 +0100

    added logger added body parser added empty routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
+var createError = require("http-errors");
 const express = require("express");
-
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 5000;
 
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("Hello");
+});
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 app.listen(port, () => console.log("Server is running"));
