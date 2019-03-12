5b7957ab4aaa6a98b7461035219a39be3bb9e946
Orhan CINAR <orhancinar35@gmail.com>
2019-03-12 01:21:33 +0100

    update main products fetch from api

--- components/Main.js
+++ components/Main.js
@@ -10,11 +10,14 @@ import {
 } from "react-native";
 import styles from "./styles/Main.style";
 
-const PRODUCTS_URL = "http://localhost:5000/products";
-const BANNERS_URL = "http://localhost:5000/banners";
+const PRODUCTS_URL = "http://192.168.1.22:5000/products";
+const BANNERS_URL = "http://192.168.1.22:5000/banners";
 
 class Main extends React.Component {
-  state = {};
+  state = {
+    products: [],
+    banners: []
+  };
   static navigationOptions = {
     title: "Best Sellers"
   };
@@ -25,7 +28,12 @@ class Main extends React.Component {
         return response.json();
       })
       .then(json => {
-        this.state.products = JSON.stringify(json);
+        this.setState({
+          products: json.data.products
+        });
+      })
+      .catch(e => {
+        console.log(e);
       });
 
     fetch(BANNERS_URL)
@@ -33,92 +41,56 @@ class Main extends React.Component {
         return response.json();
       })
       .then(json => {
-        this.state.banners = JSON.stringify(json);
+        this.setState({
+          banners: json.data.banners
+        });
+      })
+      .catch(e => {
+        console.log(e);
       });
   }
 
   render() {
+    if (this.state.products == null || this.state.products.length == 0) {
+      return (
+        <View>
+          <Text>Loading</Text>
+        </View>
+      );
+    }
+    const { products } = this.state;
+
     return (
       <View style={styles.headerContainer}>
         {/* <Text style={styles.headerText}>Most Selling Products</Text> */}
         <ScrollView>
           <View style={styles.container}>
-            <TouchableOpacity
-              onPress={() =>
-                this.props.navigation.push("Product", {
-                  itemId: Math.floor(Math.random() * 100)
-                })
-              }
-            >
-              <View style={styles.productContainer}>
-                <Image
-                  source={require("../assets/Products/Images/32740307068_01.jpg")}
-                  style={styles.productImage}
-                />
-                <View style={styles.productTextContainer}>
-                  <Text style={styles.productHeader}>Sandisk 64gb SdCard</Text>
+            {products.map(item => (
+              <TouchableOpacity
+                onPress={() =>
+                  this.props.navigation.push("Product", {
+                    itemId: item._id
+                  })
+                }
+                key={item._id}
+              >
+                <View style={styles.productContainer}>
+                  <Image
+                    url
+                    source={{ uri: item.imageUrl }}
+                    style={styles.productImage}
+                  />
+                  <View style={styles.productTextContainer}>
+                    <Text style={styles.productHeader}>{item.name}</Text>
+                  </View>
+                  <View style={styles.priceContainer}>
+                    <Text style={styles.oldPrice}>{item.oldPrice}</Text>
+                    <Text style={styles.price}>{item.price}</Text>
+                  </View>
                 </View>
-                <View style={styles.priceContainer}>
-                  <Text style={styles.oldPrice}>12.00</Text>
-                  <Text style={styles.price}>10.00</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-            <View style={styles.productContainer}>
-              <Image
-                source={require("../assets/Products/Images/32880261657_01.jpg")}
-                style={styles.productImage}
-              />
-              <View style={styles.productTextContainer}>
-                <Text style={styles.productHeader}>Lingerie</Text>
-              </View>
-              <View style={styles.priceContainer}>
-                <Text style={styles.oldPrice}>35.00</Text>
-                <Text style={styles.price}>23.00</Text>
-              </View>
-            </View>
-            <View style={styles.productContainer}>
-              <Image
-                source={require("../assets/Products/Images/32903877682_01.jpg")}
-                style={styles.productImage}
-              />
-              <View style={styles.productTextContainer} />
-              <Text style={styles.productHeader}>
-                Samsung Screen Protection
-              </Text>
-              <View style={styles.priceContainer}>
-                <Text style={styles.oldPrice}>8.99</Text>
-                <Text style={styles.price}>10.00</Text>
-              </View>
-            </View>
-            <View style={styles.productContainer}>
-              <View style={styles.productImage}>
-                <Image
-                  source={require("../assets/Products/Images/32924478819_01.jpg")}
-                  style={styles.productImage}
-                />
-                <View style={styles.productTextContainer}>
-                  <Text style={styles.productHeader}>Man Bag</Text>
-                </View>
-                <View style={styles.priceContainer}>
-                  <Text style={styles.oldPrice}>25.00</Text>
-                  <Text style={styles.price}>18.50</Text>
-                </View>
-              </View>
-            </View>
-            <View style={styles.productContainer}>
-              <Image
-                source={require("../assets/Products/Images/32947594040_01.jpg")}
-                style={styles.productImage}
-              />
-              <View style={styles.productTextContainer}>
-                <Text style={styles.productHeader}>Utilities</Text>
-              </View>
-              <View style={styles.priceContainer}>
-                <Text style={styles.oldPrice}>19.00</Text>
-                <Text style={styles.price}>14.25</Text>
-              </View>
-            </View>
+              </TouchableOpacity>
+            ))}
+            {/* 
             <View>
               <Button
                 onPress={() =>
@@ -136,7 +108,7 @@ class Main extends React.Component {
                 }
                 title="Login"
               />
-            </View>
+            </View> */}
           </View>
         </ScrollView>
       </View>
