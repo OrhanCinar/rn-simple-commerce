02d7a6227f6d14dd8a9d0cfbfdc819b7ce662fa4
Orhan CINAR <orhancinar35@gmail.com>
2019-03-14 01:17:38 +0100

    update product api

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,39 +1,52 @@
 var express = require("express");
 var router = express.Router();
 var myMongo = require("../config/mymongo");
-var ObjectId = require("mongodb").ObjectId;
-const cdnPath = "http://localhost:5000/assets/Products/Images/";
+var ObjectID = require("mongodb").ObjectID;
+const cdnPath = "http://192.168.1.22:5000/assets/Products/Images/";
 
 router.get("/product/:id", function(req, res, next) {
-  console.log("product route hit");
+  console.log("product route hit", req.params);
+
+  const { id } = req.params;
+
+  if (id === "0") {
+    res.jsonp({
+      data: {
+        status: "No Data",
+        product: {}
+      }
+    });
+  }
+
   try {
     var client = myMongo.getClient();
     client.connect((err, client) => {
       myMongo.handleConnection(err);
       const db = client.db(myMongo.dbName);
-      //console.log("id", req.params);
-      db.collection("product").findOne(
-        { _id: ObjectId(req.params.id) },
-        function(err, product) {
-          if (err) {
-            res.jsonp(err);
-          }
 
-          if (product) {
-            //console.log(result);
-            product.imageUrl = cdnPath.concat(product.imageUrl);
-            res.jsonp({
-              data: {
-                product: product
-              }
-            });
-          } else {
-            res.jsonp({
-              status: "No Product"
-            });
-          }
+      console.log("id", id);
+      db.collection("product").findOne({ _id: ObjectID(id) }, function(
+        err,
+        product
+      ) {
+        if (err) {
+          res.jsonp(err);
         }
-      );
+
+        if (product) {
+          //console.log(result);
+          product.imageUrl = cdnPath.concat(product.imageUrl);
+          res.jsonp({
+            data: {
+              product: product
+            }
+          });
+        } else {
+          res.jsonp({
+            status: "No Product"
+          });
+        }
+      });
     });
     client.close();
   } catch (error) {
