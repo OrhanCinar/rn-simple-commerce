f5efddfa809b021ceacd174577e5154ba458882f
Orhan CINAR <orhancinar35@gmail.com>
2019-03-20 00:10:25 +0100

    added new tests

--- tests/mongo.test.js
+++ tests/mongo.test.js
@@ -1,4 +1,5 @@
-var rest, mime, client;
+//github.com/cujojs/rest/blob/master/docs/interceptors.md
+https: var rest, mime, client;
 
 (rest = require("rest")), (mime = require("rest/interceptor/mime"));
 client = rest.wrap(mime);
@@ -11,14 +12,14 @@ test("Server Test", async () => {
   });
 });
 
-test("Test Products", async () => {
+test("Get Products", async () => {
   return client({ path: "http://localhost:5000/products" }).then(response => {
     expect(response).toBeDefined();
     expect(response.entity.data.status).toEqual("OK");
   });
 });
 
-test("Test Single Product", async () => {
+test("Get Single Product", async () => {
   return client({
     path: "http://localhost:5000/product/5c82fa4a5e22815492646456"
   }).then(response => {
@@ -28,9 +29,38 @@ test("Test Single Product", async () => {
   });
 });
 
-test("Test Cart", async () => {
+test("Get Cart", async () => {
   return client({ path: "http://localhost:5000/cart" }).then(response => {
     expect(response).toBeDefined();
     expect(response.entity.data.status).toEqual("OK");
+  });
+});
+
+test("Test Categories", async () => {
+  return client({ path: "http://localhost:5000/category" }).then(response => {
+    expect(response).toBeDefined();
+    expect(response.entity.data.status).toEqual("OK");
+  });
+});
+
+test("Test Login", async () => {
+  return client({
+    path: "http://localhost:5000/login",
+    method: "POST",
+    entity: { userName: "Test", password: "Test" }
+  }).then(response => {
+    expect(response).toBeDefined();
+    expect(response.entity.data.status).toEqual("NOTOK");
+  });
+});
+
+test("Test Register", async () => {
+  return client({
+    path: "http://localhost:5000/register",
+    method: "POST",
+    entity: { userName: "Test", password: "Test" }
+  }).then(response => {
+    expect(response).toBeDefined();
+    expect(response.entity.data.status).toEqual("NOTOK");
   });
 });
