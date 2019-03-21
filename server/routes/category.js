6638345c577152fde466886fdf741e59abf724fd
Orhan CINAR <orhancinar35@gmail.com>
2019-03-21 02:42:56 +0100

    update category mongo

--- server/routes/category.js
+++ server/routes/category.js
@@ -3,19 +3,27 @@ var router = express.Router();
 var myMongo = require("../config/mymongo");
 
 router.get("/category", function(req, res, next) {
+  console.log("category route hit", req.params);
   try {
     var client = myMongo.getClient();
     client.connect((err, client) => {
       myMongo.handleConnection(err);
       const db = client.db(myMongo.dbName);
-      const bannerList = db.collection("banner");
-      const categoryList = db.collection("category");
 
-      res.status(200).jsonp({
-        data: {
-          status: "OK",
-          bannerList: bannerList,
-          categoryList: categoryList
+      const collCategory = db.collection("category");
+
+      collCategory.find().toArray(function(err, result) {
+        if (err) {
+          res.status(500).jsonp(err);
+        }
+        console.log("category count", result.length);
+        if (result.length > 0) {
+          res.status(200).jsonp({
+            data: {
+              status: "OK",
+              categoryList: result
+            }
+          });
         }
       });
     });
