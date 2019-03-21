2b866c0e041f070f4b8460bbfbbdff0935c8fc9e
Orhan CINAR <orhancinar35@gmail.com>
2019-03-21 02:43:06 +0100

    update login mongo

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,32 +1,46 @@
 var express = require("express");
 var router = express.Router();
 var myMongo = require("../config/mymongo");
+const bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 
-router.post("/login", function(req, res, next) {
+router.post("/login", jsonParser, function(req, res, next) {
+  console.log("login route hit", req.body.userName, req.body.password);
   try {
     var client = myMongo.getClient();
     client.connect((err, client) => {
       myMongo.handleConnection(err);
       const db = client.db(myMongo.dbName);
 
-      var userName = req.params.userName;
-      var password = req.params.password;
+      var userName = req.body.userName;
+      var password = req.body.password;
 
       db.collection("user").findOne(
         { userName: userName, password: password },
         function(err, user) {
           if (err) {
-            res.status.jsonp({ status: "NOTOK", error: "User Not Found" });
+            res.status.jsonp({
+              data: {
+                status: "NOTOK",
+                message: "User Not Found"
+              }
+            });
           }
 
           if (user) {
             res.jsonp({
-              status: "OK",
-              user: user
+              data: {
+                status: "OK",
+                message: "User Found",
+                user: user
+              }
             });
           } else {
             res.jsonp({
-              status: "NOTOK"
+              data: {
+                status: "NOTOK",
+                message: "User Not Found"
+              }
             });
           }
         }
@@ -35,41 +49,68 @@ router.post("/login", function(req, res, next) {
       client.close();
     });
   } catch (error) {
-    res.status(500).jsonp({ status: "OK", error: "Internal Error!" });
+    res.status(500).jsonp({
+      data: {
+        status: "OK",
+        message: "Internal Error!"
+      }
+    });
   }
 });
 
-router.post("/register", function(req, res, next) {
+router.post("/register", jsonParser, function(req, res, next) {
+  console.log("register route hit", req.body.userName, req.body.password);
   try {
     var client = myMongo.getClient();
     client.connect((err, client) => {
       myMongo.handleConnection(err);
 
-      var userName = req.params.userName;
-      var password = req.params.password;
+      var userName = req.body.userName;
+      var password = req.body.password;
 
       const db = client.db(myMongo.dbName);
-      const user = db.collection("user").insertOne({
-        userName: userName,
-        password: password
-      });
+      const user = db.collection("user").insertOne(
+        {
+          userName: userName,
+          password: password
+        },
+        function(err, response) {
+          if (err) {
+            res.jsonp({
+              data: {
+                status: "NOTOK",
+                message: "User Not Registered"
+              }
+            });
+          }
 
-      if (user.acknowledged) {
-        //add user check
-        res.jsonp({
-          status: "OK",
-          status: "User Registered"
-        });
-      } else {
-        res.jsonp({
-          status: "NOTOK",
-          status: "User Not Registered"
-        });
-      }
+          if (response.insertedCount > 0) {
+            //add user check
+            res.jsonp({
+              data: {
+                status: "OK",
+                message: "User Registered"
+              }
+            });
+          } else {
+            res.jsonp({
+              data: {
+                status: "NOTOK",
+                message: "User Not Registered"
+              }
+            });
+          }
+        }
+      );
       client.close();
     });
   } catch (error) {
-    res.status(500).jsonp({ status: "NOTOK", error: "Internal Error!" });
+    res.status(500).jsonp({
+      data: {
+        status: "NOTOK",
+        message: "Internal Error!"
+      }
+    });
   }
 });
 
