370438faf57f069c20c02bb399c684a85fb93505
Orhan CINAR <orhancinar35@gmail.com>
2019-04-07 00:17:19 +0200

    update checkout api

--- server/routes/checkout.js
+++ server/routes/checkout.js
@@ -4,30 +4,19 @@ var myMongo = require("../config/mymongo");
 
 router.get("/checkout", function(req, res, next) {
   try {
-    var client = myMongo.getClient();
-    client.connect((err, client) => {
-      myMongo.handleConnection(err);
-      const db = client.db(myMongo.dbName);
-      const products = db.collection("product");
-
-      client.close();
-      //should change as list
-      res.send({
-        data: {
-          products: {
-            Id: 0,
-            ProductName: product.name,
-            Url: product.imageUrl,
-            Price: product.price,
-            OldPrice: product.oldPrice,
-            Order: product.orderId,
-            Description: product.description
-          }
-        }
-      });
-    });
+    // var client = myMongo.getClient();
+    // client.connect((err, client) => {
+    //   myMongo.handleConnection(err);
+    //   const db = client.db(myMongo.dbName);
+    //   client.close();
+    // });
   } catch (error) {
-    res.status(500).json({ error: "Internal Error!" });
+    res.status(500).jsonp({
+      data: {
+        status: "NOTOK",
+        message: "Internal Error!"
+      }
+    });
   }
 });
 
