78a7f048d210ddef33b8b60dc3d23b2818dd9429
Orhan CINAR <orhancinar35@gmail.com>
2020-03-16 16:55:11 +0100

    update cart with hooks

--- components/Cart.js
+++ components/Cart.js
@@ -1,37 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, TextInput, Image, Button, ScrollView } from "react-native";
 
 import styles from "./styles/Cart.style";
 
 import { scConfig } from "../config";
 
-class Cart extends React.Component {
-  state = {
-    products: [],
-    totals: {}
-  };
-  static navigationOptions = {
-    title: "Cart"
-  };
+function Cart() {
+  conts[(products, setProducts)] = useState([]);
+  conts[(totals, setTotals)] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
+    getCart();
+  }, []);
+
+  function getCart() {
     fetch(scConfig.CART_URL)
       .then(response => {
         return response.json();
       })
       .then(json => {
         console.log(json);
-        this.setState({
-          products: json.data.cart,
-          totals: json.data.totals
-        });
+        setProducts(json.data.cart);
+        setTotals(json.data.totals);
       })
       .catch(e => {
         console.log(e);
       });
   }
 
-  onRemoveItem() {
+  function onRemoveItem() {
     fetch(scConfig.REMOVE_FROM_CART_URL)
       .then(response => {
         return response.json();
@@ -44,74 +41,73 @@ class Cart extends React.Component {
       });
   }
 
-  render() {
-    if (this.state.products == null || this.state.products.length == 0) {
-      return (
-        <View>
-          <Text>Loading</Text>
-        </View>
-      );
-    }
-    const { products } = this.state;
+  // render() {
+  //   if (this.state.products == null || this.state.products.length == 0) {
+  //     return (
+  //       <View>
+  //         <Text>Loading</Text>
+  //       </View>
+  //     );
+  //   }
+  //   const { products } = this.state;
 
-    return (
-      <View style={styles.container}>
-        <View style={styles.scrollContainer}>
-          <ScrollView>
-            {products.map(item => (
-              <View style={styles.productContainer}>
-                <Image
-                  id={item._id}
-                  accessibilityLabel={item.name}
-                  url
-                  source={{ uri: item.imageUrl }}
-                  style={styles.productImageContainer}
-                />
+  return (
+    <View style={styles.container}>
+      <View style={styles.scrollContainer}>
+        <ScrollView>
+          {products.map(item => (
+            <View style={styles.productContainer}>
+              <Image
+                id={item._id}
+                accessibilityLabel={item.name}
+                url
+                source={{ uri: item.imageUrl }}
+                style={styles.productImageContainer}
+              />
 
-                <View style={styles.productInfoContainer}>
-                  <Text style={styles.productHeader}>{item.name}</Text>
+              <View style={styles.productInfoContainer}>
+                <Text style={styles.productHeader}>{item.name}</Text>
+              </View>
+
+              <View style={styles.priceContainer}>
+                <View style={styles.priceRow}>
+                  <Text style={styles.priceLabel}>Price :</Text>
+                  <Text style={styles.price}>{item.price}</Text>
                 </View>
 
-                <View style={styles.priceContainer}>
-                  <View style={styles.priceRow}>
-                    <Text style={styles.priceLabel}>Price :</Text>
-                    <Text style={styles.price}>{item.price}</Text>
-                  </View>
+                <View style={styles.priceRow}>
+                  <Text style={styles.priceLabel}>Quantity :</Text>
+                  <Text style={styles.price}>{item.quantity}</Text>
+                </View>
 
-                  <View style={styles.priceRow}>
-                    <Text style={styles.priceLabel}>Quantity :</Text>
-                    <Text style={styles.price}>{item.quantity}</Text>
-                  </View>
-
-                  <View style={styles.priceRow}>
-                    <Text style={styles.priceLabel}>Total :</Text>
-                    <Text style={styles.price}>{item.total}</Text>
-                  </View>
+                <View style={styles.priceRow}>
+                  <Text style={styles.priceLabel}>Total :</Text>
+                  <Text style={styles.price}>{item.total}</Text>
                 </View>
               </View>
-            ))}
-          </ScrollView>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+
+      <View style={styles.totalsContainer}>
+        <View style={styles.totalRow}>
+          <Text style={styles.priceLabel}>TOTAL :</Text>
+          <Text style={styles.price}>{this.state.totals.total}</Text>
         </View>
 
-        <View style={styles.totalsContainer}>
-          <View style={styles.totalRow}>
-            <Text style={styles.priceLabel}>TOTAL :</Text>
-            <Text style={styles.price}>{this.state.totals.total}</Text>
-          </View>
+        <View style={styles.totalRow}>
+          <Text style={styles.priceLabel}>DISCOUNT :</Text>
+          <Text style={styles.price}>{this.state.totals.discount}</Text>
+        </View>
 
-          <View style={styles.totalRow}>
-            <Text style={styles.priceLabel}>DISCOUNT :</Text>
-            <Text style={styles.price}>{this.state.totals.discount}</Text>
-          </View>
-
-          <View style={styles.totalRow}>
-            <Text style={styles.priceLabel}>SUB TOTAL :</Text>
-            <Text style={styles.price}>{this.state.totals.subTotal}</Text>
-          </View>
+        <View style={styles.totalRow}>
+          <Text style={styles.priceLabel}>SUB TOTAL :</Text>
+          <Text style={styles.price}>{this.state.totals.subTotal}</Text>
         </View>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 export default Cart;
